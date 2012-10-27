1de8214294c268c8f2d1e999ea8b1fb78c612ce8
Gulyuk <gulyuk.sergey@gmail.com>
2012-10-27 14:43:03 +0600

    re-edit config

--- one_touch_990_ALRU.mk
+++ one_touch_990_ALRU.mk
@@ -17,28 +17,16 @@
 ## (1) First, the most specific values, i.e. the aspects that are specific to GSM
 
 PRODUCT_COPY_FILES += \
-    device/qcom/one_touch_990_gsm/boot/init.rc:root/init.rc\
-    device/qcom/one_touch_990_gsm/boot/init.qcom.rc:root/init.qcom.rc\
     device/qcom/one_touch_990_gsm/boot/initlogo.rle:root/initlogo.rle
+    #device/qcom/one_touch_990_gsm/boot/init.rc:root/init.rc\
+    #device/qcom/one_touch_990_gsm/boot/init.qcom.rc:root/init.qcom.rc\
 
-# Default network type.
-# 0 => WCDMA preferred.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=0
-
-# For emmc phone storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.phone_storage = 0
-
-
-# This is a 512MB device, so 32mb heapsize
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapsize=32m
+DEVICE_PACKAGE_OVERLAYS := device/qcom/one_touch_990_gsm/overlay
 
 ## (1) First, the most specific values, i.e. the aspects that are specific to GSM
 
 ## (2) Also get non-open-source GSM-specific aspects if available
-$(call inherit-product-if-exists, vendor/qcom/brandy/brandy-vendor.mk)
+$(call inherit-product, vendor/qcom/brandy/brandy-vendor-blobs.mk)
 
 ## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
 
@@ -59,8 +47,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
 
 PRODUCT_PACKAGES += \
     librs_jni \
@@ -70,20 +57,12 @@ PRODUCT_PACKAGES += \
     copybit.msm7k \
     sensors.msm7k \
     gps.default
-#    com.android.future.usb.accessory
 
-PRODUCT_COPY_FILES += \
-    device/qcom/one_touch_990_gsm/vold.fstab:system/etc/vold.fstab \
-    device/common/gps/gps.conf:system/etc/gps.conf
+$(call inherit-product, device/common/gps/gps_us.mk)
 
 # stuff common to all phones
 $(call inherit-product, device/qcom/common/common.mk)
 
-#PRODUCT_BUILD_PROP_OVERRIDES +=  \
-#				PRODUCT_NAME=one_touch_990_ALRU \
-#				BUILD_FINGERPRINT=TCT/one_touch_990_ALRU/one_touch_990_gsm:2.3.4/GINGERBREAD/#v5H6-0:user/release-keys/release-keys \
-#				PRIVATE_BUILD_DESC="one_touch_990_ALRU-user 2.3.4 GINGERBREAD v5H6-0 release-#keys" \
-#				BUILD_NUMBER=v5H6-0
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := one_touch_990_ALRU
